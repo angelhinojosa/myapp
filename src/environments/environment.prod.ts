76a610dd96fe4917a5cbf92bd267fcf4b067179b
Angel <angelhinojosahernandez@gmail.com>
2020-07-02 19:36:38 +0200

    instalacion de fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAzCFhuHcOD7oZ4uGscF3D3f7MhiqK-zUI',
+    authDomain: 'escuelait-87390.firebaseapp.com',
+    databaseURL: 'https://escuelait-87390.firebaseio.com',
+    projectId: 'escuelait-87390',
+    storageBucket: 'escuelait-87390.appspot.com',
+    messagingSenderId: '1065230372943',
+    appId: '1:1065230372943:web:4e5279ef05b7c97df91a3a'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAzCFhuHcOD7oZ4uGscF3D3f7MhiqK-zUI',
+    authDomain: 'escuelait-87390.firebaseapp.com',
+    databaseURL: 'https://escuelait-87390.firebaseio.com',
+    projectId: 'escuelait-87390',
+    storageBucket: 'escuelait-87390.appspot.com',
+    messagingSenderId: '1065230372943',
+    appId: '1:1065230372943:web:4e5279ef05b7c97df91a3a'
+  }
 };
 
 /*
